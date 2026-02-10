a26d80e29a65708ff3f49da77a5664f4c4861530
BrunoSartos <brunobeltrame065@gmail.com>
2026-02-10 14:04:26 +0000

    exemplo1

--- exemplo1.c
+++ exemplo1.c
@@ -1,1 +1,8 @@
+#include <stdio.h>
 
+int main() {
+    printf("============================================\nBEM-VINDO AO LAB 01!\n============================================\n");
+    printf("Seu Nome: Bruno\nData: 10/02/2026\nLinguagem: C\n");
+    printf("Vamos aprender C juntos! \n=============================================\n");
+    return 0;
+}
